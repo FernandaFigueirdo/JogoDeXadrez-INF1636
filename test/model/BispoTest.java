67827468991b395e7d86fc32eac2eaa29cb3f8c1
marcela-05 <132473980+marcela-05@users.noreply.github.com>
2025-05-17 16:33:36 -0300

    Update BispoTest.java

--- test/model/BispoTest.java
+++ test/model/BispoTest.java
@@ -1,14 +1,61 @@
-package model;
-
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import model.*;
+import java.util.List;
 
 public class BispoTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private Tabuleiro tabuleiro;
 
+    @Before
+    public void setUp() {
+        tabuleiro = new Tabuleiro();
+    }
+
+    @Test(timeout = 2000)
+    public void deveMoverPraDiagonalCimaDireita() {
+        Bispo bispo = new Bispo(Cor.BRANCO, 3, 3);
+        tabuleiro.colocarPeca(bispo, 3, 3);
+
+        List<Posicao> movimentos = bispo.getMovimentosPossiveis(tabuleiro);
+
+        assertTrue("Era pra poder ir pra (2,4)", movimentos.contains(new Posicao(2, 4)));
+    }
+
+    @Test(timeout = 2000)
+    public void deveMoverPraDiagonalBaixoEsquerda() {
+        Bispo bispo = new Bispo(Cor.BRANCO, 3, 3);
+        tabuleiro.colocarPeca(bispo, 3, 3);
+
+        List<Posicao> movimentos = bispo.getMovimentosPossiveis(tabuleiro);
+
+        assertTrue("Era pra poder ir pra (4,2)", movimentos.contains(new Posicao(4, 2)));
+    }
+
+    @Test(timeout = 2000)
+    public void naoDevePassarSeTiverPecaMesmaCor() {
+        Bispo bispo = new Bispo(Cor.BRANCO, 3, 3);
+        tabuleiro.colocarPeca(bispo, 3, 3);
+
+        Peao bloqueio = new Peao(Cor.BRANCO, 2, 4);
+        tabuleiro.colocarPeca(bloqueio, 2, 4);
+
+        List<Posicao> movimentos = bispo.getMovimentosPossiveis(tabuleiro);
+
+        assertFalse("Não deveria conseguir passar da peça em (2,4)", movimentos.contains(new Posicao(2, 4)));
+    }
+
+    @Test(timeout = 2000)
+    public void deveConseguirCapturarPecaDoInimigo() {
+        Bispo bispo = new Bispo(Cor.BRANCO, 3, 3);
+        tabuleiro.colocarPeca(bispo, 3, 3);
+
+        Peao adversario = new Peao(Cor.PRETO, 2, 4);
+        tabuleiro.colocarPeca(adversario, 2, 4);
+
+        List<Posicao> movimentos = bispo.getMovimentosPossiveis(tabuleiro);
+
+        assertTrue("Deveria conseguir capturar a peça do adversário em (2,4)", movimentos.contains(new Posicao(2, 4)));
+    }
 }
